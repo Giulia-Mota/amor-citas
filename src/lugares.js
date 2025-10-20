fb142f808f67a11b979fe0c256f1de5b32a8aed0
Giulia <giuliamotasantos@gmail.com>
2025-10-20 00:39:03 -0300

    Refazendo o projeto do zero

--- src/lugares.js
+++ src/lugares.js
@@ -1,28 +1,23 @@
-// Dados dos lugares.
-// Observação: por praticidade, aqui usamos coordenadas no formato [lat, lng].
-// O Mapbox GL JS espera [lng, lat], então o app converte automaticamente.
-
-// Você pode usar um de dois formatos por lugar:
-// 1) Campo "foto" (apenas uma imagem) + "mensagem"
-// 2) Campo "fotos" (array) com objetos { url, texto } para ativar o carrossel
-// Exemplo de múltiplas fotos está no primeiro item abaixo.
-
+// Lista dos lugares que vocês visitaram 💞
 const lugares = [
   {
-    nome: "Modena Residence",
-    coordenadas: [-1.4291488184684868, -48.47065626325489],
+    nome: "Bendita Marvada",
+    coords: [-1.4344942395967275, -48.486845013491056],
     fotos: [
-      { url: "../public/img/foto 3x4 1.jpg", texto: "Nosso primeiro pôr do sol juntos 🌅" },
-      { url: "../public/img/parceiros-de-visao-lateral-de-maos-dadas-1-.avif", texto: "Nosso!!1! primeiro pôr do sol juntos 🌅" },
-      { url: "../public/img/download.jpeg", texto: "Nosso!1!1! primeiro pôr do sol juntos 🌅" },
-      { url: "../public/img/download (2).jpeg", texto: "Nosso!!111! primeiro pôr do sol juntos 🌅" },
-
-    ],
-    encontro: true,
+      { imagem: "../public/img/IMG_20241226_203017.jpg", 
+        texto: "Nosso primeiro piquenique 💗" },
+      { imagem: "../public/img/Screenshot_2025-06-27-09-24-49-221_com.miui.gallery.jpg", 
+        texto: "O pôr do sol mais lindo do mundo 🌅" }
+    ]
   },
+  {
+    nome: "Módena Residence",
+    coords: [-1.4289472390257292, -48.47062790674626],
+    fotos: [
+      { imagem: "../public/img/IMG_20241230_122501.jpg", 
+        texto: "Você rindo de algo que eu disse 💕" },
+      { imagem: "../public/img/IMG_20241230_122513.jpg", 
+        texto: "Nosso cappuccino favorito ☕✨" }
+    ]
+  }
 ];
-
-// Exporte global para o script principal
-window.lugares = lugares;
-
-
